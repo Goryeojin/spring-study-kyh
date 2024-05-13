ac76e2914c87dabf5695a5a46e34dc32ed09bcf9
ryeojin <sooon9254@naver.com>
2024-05-13 21:43:06 +0900

    @ComponentScan 의존 관계 자동 주입

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class CoreApplication {
 
 	public static void main(String[] args) {
