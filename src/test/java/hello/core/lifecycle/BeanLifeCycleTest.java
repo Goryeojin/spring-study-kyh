9abf639697ab3132c542436e6a67d6c2dfac83fb
ryeojin <sooon9254@naver.com>
2024-07-16 22:41:34 +0900

    빈 등록 및 소멸시 콜백 1. implements InitializingBean 2. DisposableBean

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,13 +1,12 @@
 package hello.core.lifecycle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-public class BeanLifeCycle {
+public class BeanLifeCycleTest {
 
     @Test
     public void lifeCycleTest() {
