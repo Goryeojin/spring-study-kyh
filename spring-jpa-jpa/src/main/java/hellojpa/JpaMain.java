8eb83d4bcd09c9eee288ba3145e555e4b07db0fd
ryeojin <sooon9254@naver.com>
2024-09-06 00:03:11 +0900

    add: 영속성 컨텍스트 테스트

--- spring-jpa-jpa/src/main/java/hellojpa/JpaMain.java
+++ spring-jpa-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,11 +15,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = em.find(Member.class, 150L);
-            member.setName("zzz");
-//            em.persist(member);
-            System.out.println("==============");
+            Member member = em.find(Member.class, 3L);
+            member.setName("AAAA");
 
+            em.clear();
+
+            Member member1 = em.find(Member.class, 3L);
+            System.out.println(member == member1);
+
+            System.out.println("===================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
